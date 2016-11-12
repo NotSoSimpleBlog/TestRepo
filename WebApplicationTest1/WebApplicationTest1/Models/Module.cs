2e4ba313ad0e2bacfadd86b3795cf6156654b0db
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-11-12 21:56:56 +0200

    TestWebApplication

---
+++ WebApplicationTest1/WebApplicationTest1/Models/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationTest1.Models
+{
+    public class Module
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class SolutionsDBContext : DbContext
+    {
+        public DbSet<Module> Modules { get; set; }
+    }
+}
