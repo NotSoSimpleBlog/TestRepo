058c0fb77de50a77d394457b79745b2f49b9f5b9
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-12-04 18:42:28 +0200

    commit.4.12.2016

--- WebApplicationTest1/WebApplicationTest1/Controllers/HomeController.cs
+++ WebApplicationTest1/WebApplicationTest1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplicationTest1.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Modules");
         }
 
         public ActionResult About()
